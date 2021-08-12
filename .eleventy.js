aafa38b43199abbdf358c7dadd41af8b975161f9
Amrita <amrita.chanda@gmail.com>
2021-08-12 11:26:11 +0530

    DONE: Homepage without testimonials, and gods-flickity

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/utils");
 
   eleventyConfig.addShortcode("youtube", (id) => {
-    return `<div class="video-wrapper"><iframe src="https://www.youtube.com/embed/${id}" frameborder="0" allowfullscreen allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe></div>`;
+    return `<div class="video-container"><iframe class="responsive-iframe" src="https://www.youtube.com/embed/${id}" frameborder="0" allowfullscreen allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe></div>`;
   });
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
